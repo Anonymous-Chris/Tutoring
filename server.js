0dd20a8e353ae986f19a2a2409431698f13dc488
Anonymous-Chris <35816754+Anonymous-Chris@users.noreply.github.com>
2020-04-19 02:40:54 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'build')));
 
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
